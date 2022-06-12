d5b132a777870946d9c439e0d3334246f9617361
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-12 11:43:21 +0300

    created new url for accounts login

--- myAwwards/urls.py
+++ myAwwards/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'^Awwards/',include('Awwards.urls'))
+    re_path(r'',include('Awwards.urls'))
 ]
 
 
