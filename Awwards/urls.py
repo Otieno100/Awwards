9229d576c4ede81d18cf5f27256b02315fdfdbc7
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-13 23:47:40 +0300

    added new url for profile page

--- Awwards/urls.py
+++ Awwards/urls.py
@@ -6,11 +6,14 @@ from django.conf.urls.static import static
 urlpatterns=[
     # re_path('^$',views.post,name = 'newpost'),
     re_path(r'^$',views.post_item,name='postToday'),
+    re_path(r'^accounts/profile/$',views.profile,name='profile_edit'),
     re_path(r'^post/(\d+)',views.post_item,name ='post'),
     re_path(r'^search/', views.search_results, name='search_results'),
-    re_path(r'^new/updateUser$', views.updateUser, name='updateUser'),
-    re_path(r'^profile/(?P<username>\w+)/settings', views.updateProfile, name='updateProfile'),
-    re_path(r'^api/merch/$', views.MerchList.as_view())
+    re_path(r'^new/updateUser$', views.updatePost, name='updateUser'),
+    # re_path(r'^profile/(?P<username>\w+)/settings', add_profile, name='Profile'),
+    re_path(r'^api/merch/$', views.MerchList.as_view()),
+    re_path(r"^/profile/(\d+)", views.add_profile, name="profile"),
+    # re_path(r'^project/(?P<id>\d+)',views.project,name='project'),
     
 ]
 
