b3d0e184bcc6a22b6e1565d2023a0e99b01faccc
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-13 23:48:41 +0300

    added views for profile

--- Awwards/views.py
+++ Awwards/views.py
@@ -18,7 +18,7 @@ from rest_framework.views import APIView
 from .models import  AwwardsMerch
 from .serializer import MerchSerializer
 from rest_framework import status
-from .forms import PostForm,UpdateUserForm,UpdateUserProfileForm,RatingsForm,SignupForm
+from .forms import UpdateUserForm,UpdateUserProfileForm
 from .models import Post,Profile,Rating
 
 
@@ -64,11 +64,49 @@ def signUp(request,post_id):
         raise Http404()
     return render(request,"awwards/post.html", {"post":post})
 
+# def profile (request):
+#     prof = Profile.objects.all()
+#     return (request, "updateProfile.html", {prof:"prof"})
 
+
+    # 
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    owner = current_user
+    projects = Post.get_by_user(owner)
+    if request.method == 'POST':
+        form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+        return redirect('profile')
+    else:
+        form = UpdateUserProfileForm()
+    return render(request, 'registration/updateProfile.html', {"form":form, "projects":projects})
+
+# 
+
+
+
+def add_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form =  UpdateUserProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('postToday')
+
+    else:
+        form =  UpdateUserProfileForm()
+    return render(request, 'profile.html', {"form": form})
 
 
 @login_required(login_url='/accounts/login/')
-def updateUser(request):
+def updatePost(request):
     current_user = request.user
     if request.method == 'POST':
         form = NewAwwardsForm(request.POST, request.FILES)
@@ -84,25 +122,6 @@ def updateUser(request):
 
 
 
-def updateProfile(request, username):
-    user = User.objects.get(username=username)
-    if request.method == 'POST':
-        user_form = UpdateUserForm(request.POST, instance=request.user)
-        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
-        if user_form.is_valid() and prof_form.is_valid():
-            user_form.save()
-            prof_form.save()
-            return redirect('profile', user.username)
-    else:
-        user_form = UpdateUserForm(instance=request.user)
-        prof_form = UpdateUserProfileForm(instance=request.user.profile)
-    params = {
-        'user_form': user_form,
-        'prof_form': prof_form
-    }
-    return render(request, 'profile.html', params)
-
-
 
 
 
@@ -114,54 +133,54 @@ class MerchList(APIView):
 
     # permission_classes = (IsAdminOrReadOnly,) 
     def post(self, request, format=None):
-            serializers = MerchSerializer(data=request.data)
-            if serializers.is_valid():
-                  serializers.save()
-                  return Response(serializers.data, status=status.HTTP_201_CREATED)
-            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializers = MerchSerializer(data=request.data)
+        if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
  
 
-def project(request, id):
-    post = Post.objects.get(id=id)
-    ratings = Rating.objects.filter(user=request.user, post=post).first()
-    rating_status = None
-    if ratings is None:
-        rating_status = False
-    else:
-        rating_status = True
-    if request.method == 'POST':
-        form = RatingsForm(request.POST)
-        if form.is_valid():
-            rate = form.save(commit=False)
-            rate.user = request.user
-            rate.post = post
-            rate.save()
-            post_ratings = Rating.objects.filter(post=post)
+# def ratings(request, id):
+#     post = Post.objects.get(id=id)
+#     ratings = Rating.objects.filter(user=request.user, post=post).first()
+#     rating_status = None
+#     if ratings is None:
+#         rating_status = False
+#     else:
+#         rating_status = True
+#     if request.method == 'POST':
+#         form = RatingsForm(request.POST)
+#         if form.is_valid():
+#             rate = form.save(commit=False)
+#             rate.user = request.user
+#             rate.post = post
+#             rate.save()
+#             post_ratings = Rating.objects.filter(post=post)
 
-            design_ratings = [d.design for d in post_ratings]
-            design_average = sum(design_ratings) / len(design_ratings)
+#             design_ratings = [d.design for d in post_ratings]
+#             design_average = sum(design_ratings) / len(design_ratings)
 
-            usability_ratings = [us.usability for us in post_ratings]
-            usability_average = sum(usability_ratings) / len(usability_ratings)
+#             usability_ratings = [us.usability for us in post_ratings]
+#             usability_average = sum(usability_ratings) / len(usability_ratings)
 
-            content_ratings = [content.content for content in post_ratings]
-            content_average = sum(content_ratings) / len(content_ratings)
+#             content_ratings = [content.content for content in post_ratings]
+#             content_average = sum(content_ratings) / len(content_ratings)
 
-            score = (design_average + usability_average + content_average) / 3
-            print(score)
-            rate.design_average = round(design_average, 2)
-            rate.usability_average = round(usability_average, 2)
-            rate.content_average = round(content_average, 2)
-            rate.score = round(score, 2)
-            rate.save()
-            return HttpResponseRedirect(request.path_info)
-    else:
-        form = RatingsForm()
-    params = {
-        'post': post,
-        'rating_form': form,
-        'rating_status': rating_status
+#             score = (design_average + usability_average + content_average) / 3
+#             print(score)
+#             rate.design_average = round(design_average, 2)
+#             rate.usability_average = round(usability_average, 2)
+#             rate.content_average = round(content_average, 2)
+#             rate.score = round(score, 2)
+#             rate.save()
+#             return HttpResponseRedirect(request.path_info)
+#     else:
+#         form = RatingsForm()
+#     params = {
+#         'post': post,
+#         'rating_form': form,
+#         'rating_status': rating_status
 
-    }
-    return render(request, 'project.html', params)              
+#     }
+#     return render(request, 'project.html', params)              
