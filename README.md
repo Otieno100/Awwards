73da2100338c7ba638acfe5f3f03324d23b5d2cf
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-13 23:49:28 +0300

    added untracked files from environment

---
+++ README.md
@@ -0,0 +1,17 @@
+## Project Title
+Awwards
+
+### Author
+Brian Otieno
+
+### Description
+This is a Django application for displaying different projects and different users can vote on them based on their usability,design and content.
+
+
+###  User Story
+
+** View posted projects and their details
+** Post a project to be rated/reviewed
+** Search for projects
+** View projects overall score
+** View my profile page
--- myAwwards/urls.py
+++ myAwwards/urls.py
@@ -3,15 +3,16 @@ from django.urls import include, re_path
 from django.contrib.auth import views
 from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'',include('Awwards.urls')),
     re_path(r'^accounts/', include('registration.backends.simple.urls')),
-    # re_path(r'^login/$', views.LogintView.as_view(), {"next_page": 'Logout'}),
-    re_path(r'^logout/$', views.LogoutView.as_view(), {"next_page": '/'}),
+    # re_path(r'^logout/$', views.LogoutView.as_view(), {"next_page": '/'}),
     re_path(r'^tinymce/', include('tinymce.urls')),
-    re_path(r'^api-token-auth/', obtain_auth_token)
+    re_path(r'^api-token-auth/', obtain_auth_token),
+    re_path(r'^logout/', auth_views.logout_then_login),
   
 ]
 
