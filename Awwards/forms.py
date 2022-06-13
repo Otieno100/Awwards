4a972eae2c9901d33aef51fcbbe313f1220e8bb1
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-13 12:44:12 +0300

    added a form.py file

---
+++ Awwards/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Post, Profile
+# from pillow.forms import ImageField
+
+
+
+class AwwardsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+
+class NewAwwardsForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['editor', 'pub_date']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+
+
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'bio', 'contact']
+
